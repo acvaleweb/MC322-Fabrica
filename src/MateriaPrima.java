7b6cf81916396a8680a595cf09275b47fae04902
Antonio Vale <antocyrineu@gmail.com>
2026-08-31 22:19:10 -0300

    comecar a fazer classe MateriaPrima - 70% feito

--- src/MateriaPrima.java
+++ src/MateriaPrima.java
@@ -1,3 +1,45 @@
 public class MateriaPrima {
 
+	// atributos obrigatorios
+	private String id;
+	private String nome;
+	private double quantidade;
+	private String unidade;
+
+	// menor lote possivel do fornecedor
+	private int quantidadeMinima;
+
+
+	// metodos publicos
+	// metodo consumir - reduz qtd em estoque de acordo com a quantidade demandada
+	public void consumir(double quantidadeDemanda) {
+		if(quantidade - quantidadeDemanda >= quantidadeMinima) {
+			quantidade = quantidade - quantidadeDemanda;
+		}
+	}
+
+	// metodo adicionarEstoque - adiciona qtd de materia prima ao estoque
+	public void adicionarEstoque(double quantidadeAdiciona) {
+		if
+		quantidade = quantidade + quantidadeAdiciona;
+	}
+
+	// metodo verificarDisponibilidade - verifica se há qtd para uma demanda
+	//
+	//arrumar - ver nome do metodo de class produto para arrumar
+	public void verificarDisponibilidade(int quantidadeProdutoFinal) {
+		if(quantidadeProdutquantidade){
+		
+		}
+	}
+
+	// metodo getId - retorna id
+	public string getId() {
+		return id;
+	}
+
+	// metodo getQuantidade - retorna quantiadade disponivel
+	
+	public float =
+
 }
